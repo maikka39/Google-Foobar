819d81a30d9708efa87a7d7e882637b27a26e3ae
Maik de Kruif <maik@maik.dev>
2021-05-04 21:36:00 +0200

    Cleanup code

--- Level 2/En Route Salute/solution.py
+++ Level 2/En Route Salute/solution.py
@@ -1,6 +1,7 @@
 def solution(s):
     to_salute = 0
-    counter = 0  # Should have been a counter
+    counter = 0
+
     for i, ch in enumerate(s):
         if ch == "-":
             continue
@@ -8,6 +9,7 @@ def solution(s):
             counter += 1
         else:
             to_salute += counter
+
     return to_salute*2
 
 
